aa96f43faabcd8b31f295a83cd71609022510f17
Attila Molnar <shovelend@gmail.com>
2024-12-07 00:19:30 +0100

    semi-working d6p2 At least it's not looping infinitely or taking >30min

--- 2024/day6/part2/main.go
+++ 2024/day6/part2/main.go
@@ -1,5 +1,6 @@
 package main
 
+//4655 TOO HIGH
 import (
 	"fmt"
 	"os"
@@ -7,105 +8,99 @@ import (
 	"strings"
 )
 
+type Pos struct {
+	x int
+	y int
+}
+
 func main() {
-	table := parseInput()
 	origTable := parseInput()
-	infinite := 0
+	traversedPath := getTraversedPath()
 	guardChars := []rune{'^', '>', 'v', '<'}
+	infinite := 0
 
-	for io, rowo := range origTable {
-		for jo := range rowo {
-			guardOnMap := true
-			posVisited := 0
+	for _, pos := range traversedPath {
+		guardOnMap := true
 
-			tmp := origTable[io][jo]
-			if !slices.Contains(guardChars, origTable[io][jo]) {
-				table[io][jo] = '#'
+		beenThereDoneThat := make([][][4]int, len(origTable))
+		for i := range origTable {
+			beenThereDoneThat[i] = make([][4]int, len(origTable[i]))
+			for j := range origTable[i] {
+				beenThereDoneThat[i][j][0] = 0
+				beenThereDoneThat[i][j][1] = 0
+				beenThereDoneThat[i][j][2] = 0
+				beenThereDoneThat[i][j][3] = 0
 			}
-			// TODO fix infinite loop
-			for guardOnMap {
-				fmt.Println(io, jo)
-				fmt.Println(posVisited)
-				if posVisited > 10000 {
-					infinite += 1
-					break
-				}
-				for i, row := range table {
-					for j := range row {
-						guardFace := slices.Index(guardChars, table[i][j])
-						if guardFace != -1 {
-							switch guardFace {
-							case 0:
-								if i == 0 {
-									setVisitedInc(table, i, j, &posVisited)
-									guardOnMap = false
-									continue
-								}
-								if table[i-1][j] != '#' {
-									table[i-1][j] = guardChars[0]
-									setVisitedInc(table, i, j, &posVisited)
-								} else {
-									table[i][j] = guardChars[1]
-								}
-								continue
-							case 1:
-								if j == len(table[i])-1 {
-									setVisitedInc(table, i, j, &posVisited)
-									guardOnMap = false
-									continue
-								}
-								if table[i][j+1] != '#' {
-									table[i][j+1] = guardChars[1]
-									setVisitedInc(table, i, j, &posVisited)
-								} else {
-									table[i][j] = guardChars[2]
-								}
-								continue
-							case 2:
-								if i == len(table)-1 {
-									setVisitedInc(table, i, j, &posVisited)
-									guardOnMap = false
-									continue
-								}
-								if table[i+1][j] != '#' {
-									table[i+1][j] = guardChars[2]
-									setVisitedInc(table, i, j, &posVisited)
-								} else {
-									table[i][j] = guardChars[3]
-								}
-								continue
-							case 3:
-								if j == 0 {
-									setVisitedInc(table, i, j, &posVisited)
-									guardOnMap = false
-									continue
-								}
-								if table[i][j-1] != '#' {
-									table[i][j-1] = guardChars[3]
-									setVisitedInc(table, i, j, &posVisited)
-								} else {
-									table[i][j] = guardChars[0]
-								}
-								continue
+		}
+		table := parseInput()
+		if !slices.Contains(guardChars, origTable[pos.x][pos.y]) && origTable[pos.x][pos.y] != '#' {
+			table[pos.x][pos.y] = '#'
+		} else {
+			continue
+		}
+		for guardOnMap {
+			for i, rows := range table {
+				for j := range rows {
+					guardFace := slices.Index(guardChars, table[i][j])
+					if guardFace != -1 {
+						if beenThereDoneThat[i][j][guardFace] > 0 {
+							infinite += 1
+							guardOnMap = false
+							break
+						}
+						switch guardFace {
+						case 0:
+							if i == 0 {
+								guardOnMap = false
+								break
+							}
+							if table[i-1][j] != '#' {
+								table[i-1][j] = guardChars[0]
+								beenThereDoneThat[i][j][guardFace] = 1
+							} else {
+								table[i][j] = guardChars[1]
+							}
+						case 1:
+							if j == len(table[i])-1 {
+								guardOnMap = false
+								break
+							}
+							if table[i][j+1] != '#' {
+								table[i][j+1] = guardChars[1]
+								beenThereDoneThat[i][j][guardFace] = 1
+							} else {
+								table[i][j] = guardChars[2]
+							}
+						case 2:
+							if i == len(table)-1 {
+								guardOnMap = false
+								break
+							}
+							if table[i+1][j] != '#' {
+								table[i+1][j] = guardChars[2]
+								beenThereDoneThat[i][j][guardFace] = 1
+							} else {
+								table[i][j] = guardChars[3]
+							}
+						case 3:
+							if j == 0 {
+								guardOnMap = false
+								break
+							}
+							if table[i][j-1] != '#' {
+								table[i][j-1] = guardChars[3]
+								beenThereDoneThat[i][j][guardFace] = 1
+							} else {
+								table[i][j] = guardChars[0]
 							}
 						}
 					}
 				}
 			}
-			origTable[io][jo] = tmp
 		}
 	}
 
 	fmt.Println(infinite)
-}
-
-func setVisitedInc(table [][]rune, i, j int, posVisited *int) {
-	if table[i][j] != '#' {
-		if table[i][j] != 'X' {
-			*posVisited += 1
-		}
-		table[i][j] = 'X'
-	}
 }
 
 func parseInput() [][]rune {
@@ -122,4 +117,85 @@ func parseInput() [][]rune {
 		}
 	}
 	return table
+}
+
+func getTraversedPath() []Pos {
+	table := parseInput()
+	guardOnMap := true
+	guardChars := []rune{'^', '>', 'v', '<'}
+
+	for guardOnMap {
+		for i, row := range table {
+			for j := range row {
+				guardFace := slices.Index(guardChars, table[i][j])
+				if guardFace != -1 {
+					switch guardFace {
+					case 0:
+						if i == 0 {
+							table[i][j] = 'X'
+							guardOnMap = false
+							continue
+						}
+						if table[i-1][j] != '#' {
+							table[i-1][j] = guardChars[0]
+							table[i][j] = 'X'
+						} else {
+							table[i][j] = guardChars[1]
+						}
+						continue
+					case 1:
+						if j == len(table[i])-1 {
+							table[i][j] = 'X'
+							guardOnMap = false
+							continue
+						}
+						if table[i][j+1] != '#' {
+							table[i][j+1] = guardChars[1]
+							table[i][j] = 'X'
+						} else {
+							table[i][j] = guardChars[2]
+						}
+						continue
+					case 2:
+						if i == len(table)-1 {
+							table[i][j] = 'X'
+							guardOnMap = false
+							continue
+						}
+						if table[i+1][j] != '#' {
+							table[i+1][j] = guardChars[2]
+							table[i][j] = 'X'
+						} else {
+							table[i][j] = guardChars[3]
+						}
+						continue
+					case 3:
+						if j == 0 {
+							table[i][j] = 'X'
+							guardOnMap = false
+							continue
+						}
+						if table[i][j-1] != '#' {
+							table[i][j-1] = guardChars[3]
+							table[i][j] = 'X'
+						} else {
+							table[i][j] = guardChars[0]
+						}
+						continue
+					}
+				}
+			}
+		}
+	}
+
+	traversedPath := []Pos{}
+	for i, row := range table {
+		for j := range row {
+
+			if table[i][j] == 'X' {
+				traversedPath = append(traversedPath, Pos{x: i, y: j})
+			}
+		}
+	}
+	return traversedPath
 }
