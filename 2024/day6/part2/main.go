91bf61e3ba90566c7edf1454aa5024c3cbeb91f4
shovelend <shovelend@gmail.com>
2024-12-09 16:15:59 +0000

    :sparkles: Complete day 6 and 7

--- 2024/day6/part2/main.go
+++ 2024/day6/part2/main.go
@@ -1,6 +1,5 @@
 package main
 
-//4655 TOO HIGH
 import (
 	"fmt"
 	"os"
@@ -18,6 +17,7 @@ const (
 	EAST
 	SOUTH
 	WEST
+	GONE
 )
 
 func main() {
@@ -25,23 +25,26 @@ func main() {
 	traversedPath := getTraversedPath()
 	guardChars := []rune{'^', '>', 'v', '<'}
 	infinite := 0
+	flaggedPoints := []Pos{}
 
 	for _, pos := range traversedPath {
 		guardOnMap := true
 		table := parseInput()
 
-		beenThereDoneThat := make([][][4]int, len(origTable))
+		beenThereDoneThat := make([][][5]int, len(origTable))
 		for i := range origTable {
-			beenThereDoneThat[i] = make([][4]int, len(origTable[i]))
+			beenThereDoneThat[i] = make([][5]int, len(origTable[i]))
 			for j := range origTable[i] {
 				beenThereDoneThat[i][j][0] = 0
 				beenThereDoneThat[i][j][1] = 0
 				beenThereDoneThat[i][j][2] = 0
 				beenThereDoneThat[i][j][3] = 0
+				beenThereDoneThat[i][j][4] = 0
 			}
 		}
 		if !slices.Contains(guardChars, origTable[pos.x][pos.y]) && origTable[pos.x][pos.y] != '#' {
 			table[pos.x][pos.y] = '#'
+			fmt.Println("Putting # to", pos)
 		} else {
 			continue
 		}
@@ -50,69 +53,82 @@ func main() {
 			for i, rows := range table {
 				for j := range rows {
 					guardDirection := slices.Index(guardChars, table[i][j])
-					if guardDirection != -1 {
-						if beenThereDoneThat[i][j][guardDirection] > 0 {
+					if guardDirection == -1 {
+						guardDirection = 4
+					}
+					if beenThereDoneThat[i][j][guardDirection] > 1 {
+						if !slices.Contains(flaggedPoints, pos) {
+							flaggedPoints = append(flaggedPoints, pos)
 							infinite += 1
 							guardOnMap = false
 							break
 						}
-						switch guardDirection {
-						case NORTH:
-							if i == 0 {
-								guardOnMap = false
-								break
-							}
-							if table[i-1][j] != '#' {
-								table[i-1][j] = guardChars[0]
-								beenThereDoneThat[i][j][guardDirection] = 1
-							} else {
-								table[i][j] = guardChars[1]
-							}
-						case EAST:
-							if j == len(table[i])-1 {
-								guardOnMap = false
-								break
-							}
-							if table[i][j+1] != '#' {
-								table[i][j+1] = guardChars[1]
-								beenThereDoneThat[i][j][guardDirection] = 1
-							} else {
-								table[i][j] = guardChars[SOUTH]
-							}
-						case SOUTH:
-							if i == len(table)-1 {
-								guardOnMap = false
-								break
-							}
-							if table[i+1][j] != '#' {
-								table[i+1][j] = guardChars[2]
-								beenThereDoneThat[i][j][guardDirection] = 1
-							} else {
-								table[i][j] = guardChars[WEST]
-							}
-						case WEST:
-							if j == 0 {
-								guardOnMap = false
-								break
-							}
-							if table[i][j-1] != '#' {
-								table[i][j-1] = guardChars[3]
-								beenThereDoneThat[i][j][guardDirection] = 1
-							} else {
-								table[i][j] = guardChars[NORTH]
-							}
+					}
+					switch guardDirection {
+					case NORTH:
+						if i == 0 {
+							guardOnMap = false
+							table[i][j] = 'X'
+							break
 						}
+						if table[i-1][j] != '#' {
+							table[i-1][j] = guardChars[0]
+							table[i][j] = 'X'
+							beenThereDoneThat[i][j][guardDirection] += 1
+						} else {
+							table[i][j] = guardChars[1]
+						}
+					case EAST:
+						if j == len(table[i])-1 {
+							table[i][j] = 'X'
+							guardOnMap = false
+							break
+						}
+						if table[i][j+1] != '#' {
+							table[i][j+1] = guardChars[1]
+							table[i][j] = 'X'
+							beenThereDoneThat[i][j][guardDirection] += 1
+						} else {
+							table[i][j] = guardChars[SOUTH]
+						}
+					case SOUTH:
+						if i == len(table)-1 {
+							guardOnMap = false
+							break
+						}
+						if table[i+1][j] != '#' {
+							table[i+1][j] = guardChars[2]
+							table[i][j] = 'X'
+							beenThereDoneThat[i][j][guardDirection] += 1
+						} else {
+							table[i][j] = guardChars[WEST]
+						}
+					case WEST:
+						if j == 0 {
+							guardOnMap = false
+							table[i][j] = 'X'
+							break
+						}
+						if table[i][j-1] != '#' {
+							table[i][j-1] = guardChars[3]
+							table[i][j] = 'X'
+							beenThereDoneThat[i][j][guardDirection] += 1
+						} else {
+							table[i][j] = guardChars[NORTH]
+						}
+					case GONE:
+						break
 					}
 				}
 			}
 		}
-	}
 
+	}
 	fmt.Println(infinite)
 }
 
 func parseInput() [][]rune {
-	content, err := os.ReadFile("../test.txt")
+	content, err := os.ReadFile("../input.txt")
 	if err != nil {
 	}
 	lines := strings.Split(string(content), "\n")
@@ -199,11 +215,17 @@ func getTraversedPath() []Pos {
 	traversedPath := []Pos{}
 	for i, row := range table {
 		for j := range row {
-
 			if table[i][j] == 'X' {
 				traversedPath = append(traversedPath, Pos{x: i, y: j})
+				// fmt.Print("X")
+			} else if table[i][j] == '#' {
+				// fmt.Print("#")
+			} else {
+				// fmt.Print(".")
 			}
 		}
+		// fmt.Println()
 	}
+
 	return traversedPath
 }
---
+++ 2024/day7/part1/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Combine struct {
+	target  int64
+	numbers []int64
+}
+
+func main() {
+	table := parseInput()
+	total := int64(0)
+	for _, combine := range table {
+		if equals(combine, 0) {
+			total += (combine.target)
+		}
+	}
+	fmt.Println(total)
+}
+
+func equals(c Combine, total int64) bool {
+	if total > c.target {
+		return false
+	}
+	if len(c.numbers) == 0 {
+		return total == c.target
+	}
+	currHead := c.numbers[0]
+	c = Combine{target: c.target, numbers: c.numbers[1:len(c.numbers)]}
+	return equals(c, total+currHead) || equals(c, total*currHead)
+}
+
+func parseInput() []Combine {
+	content, err := os.ReadFile("../input.txt")
+	if err != nil {
+	}
+	lines := strings.Split(string(content), "\n")
+
+	table := make([]Combine, len(lines))
+	for i, line := range lines {
+		splintered := strings.Split(line, ":")
+		nums := parseNums(splintered[1])
+		res, _ := strconv.Atoi(splintered[0])
+		table[i] = Combine{target: int64(res), numbers: nums}
+	}
+	return table
+}
+
+func parseNums(nums string) []int64 {
+
+	strs := strings.Split(nums, " ")
+	ary := make([]int64, len(strs))
+	for i := range ary {
+		res, _ := strconv.Atoi(strs[i])
+		ary[i] = int64(res)
+	}
+
+	return ary
+}
